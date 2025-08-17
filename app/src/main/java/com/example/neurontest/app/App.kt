2cc005918458c3bcc2bb656cbfe4eb9b1480bde7
Binar <yardemyd@gmail.com>
2025-08-17 15:30:15 +0200

    Production

--- app/src/main/java/com/example/neurontest/app/App.kt
+++ app/src/main/java/com/example/neurontest/app/App.kt
@@ -1,6 +1,7 @@
 package com.example.neurontest.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.neurontest.app.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -8,6 +9,7 @@ import org.koin.core.context.startKoin
 class NeuronTestApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         startKoin {
             androidContext(this@NeuronTestApp)
             modules(appModules)
--- app/src/main/java/com/example/neurontest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/neurontest/ui/theme/Theme.kt
@@ -1,17 +1,9 @@
 package com.example.neurontest.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = White,
@@ -22,36 +14,14 @@ private val LightColorScheme = lightColorScheme(
     surface = Purple60,
     surfaceContainerHighest = Purple30,
     outline = Black
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun NeuronTestTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
